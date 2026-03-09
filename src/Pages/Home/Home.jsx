7af7814fd625e296a2524b7918a70032f345ab1a
4020uncho <uncholimbu5@gmail.com>
2026-03-09 22:53:17 +0545

    fix: align header logo and text side by side using flexbox

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,16 +3,15 @@ import reactLogo from "../../assets/logo.png";
 const Home = () => {
   return (
     <header className="header">
-    <div>
-      <img src={reactLogo} alt="React Logo" width="200" />
-     <h1>Institute of science and technology</h1>  
-     <h2>Department of IT</h2>
-     <h3>Central campus of Technology</h3>
-     <h4>Dharan-14, Sunsari </h4>
-    </div>
+      <img src={reactLogo} alt="React Logo" width="100" />
+      <div>
+        <h1 style={{ color: "black", fontSize: "1.8em" }}>Institute of Science and Technology</h1>
+        <h2 style={{ color: "#cc4400", fontSize: "1em", fontWeight: "normal" }}>Department of IT</h2>
+        <h3 style={{ color: "#cc4400", fontSize: "0.9em", fontWeight: "normal" }}>Central Campus of Technology</h3>
+        <h4 style={{ color: "#cc4400", fontSize: "0.85em", fontWeight: "normal" }}>Dharan-14, Sunsari</h4>
+      </div>
     </header>
-    
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
