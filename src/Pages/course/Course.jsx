bdf5f461854a5fe60da29dfcb893d7a0a900ca15
4020uncho <uncholimbu5@gmail.com>
2026-05-09 22:06:06 +0545

    Layout changes

--- src/Pages/course/Course.jsx
+++ src/Pages/course/Course.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import "./Course.css";
 
 const Course = () => {
   return (
-    <div>
-      <div className='Container'>Contents</div>
+    <div className="course-page">
+      <div className="course-container">
+        <div className='Container'>Contents</div>
+        <p>Course content will be displayed here.</p>
+      </div>
     </div>
   )
 }
