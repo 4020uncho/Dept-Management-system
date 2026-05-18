58d2595ff3bf6b93e90273a572f4b383797170e3
4020uncho <uncholimbu5@gmail.com>
2026-05-18 22:20:13 +0545

    UI changes on course field

--- src/Pages/course/Course.jsx
+++ src/Pages/course/Course.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import "./Course.css";
 
 const Course = () => {
   return (
     <div className="course-page">
       <div className="course-container">
-        <div className='Container'>Contents</div>
+        <div className="button">
+          <Link to="/studentlogin/dashboard"><button aria-label="Back to Dashboard">←</button></Link>
+        </div>
+        <div className="Container">Course Dashboard</div>
         <p>Course content will be displayed here.</p>
       </div>
     </div>
